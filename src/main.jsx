a6d048dca76a4b7ad97cf7dcfe19b31832427825
jonas-45 <jonaswullah@gmail.com>
2023-02-25 16:46:48 +0000

    Add TodoApp component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import TodoApp from "./components/TodoApp";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const domContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(domContainer);
+root.render(<TodoApp />);
