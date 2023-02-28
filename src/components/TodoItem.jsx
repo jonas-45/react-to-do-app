83cc9617131e94436cc3b7cf711ba1f7b3774072
jonas-45 <jonaswullah@gmail.com>
2023-02-28 05:17:37 +0000

    Add edit task input field with styles

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import styles from '@/styles/TodoItem.module.css';
 
 const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+  const [editing, setEditing] = useState(false);
+
+  const handleEditing = () => {
+    setEditing(!editing);
+  }
+  const viewMode = {};
+  const editMode = {};
+  if(editing) {
+    viewMode.display = 'none';
+  }else{
+    editMode.display = 'none';
+  }
   const completedStyle = {
     fontStyle: 'italic',
     color: '#595959',
@@ -10,12 +23,14 @@ const TodoItem = ({ itemProp, handleChange, delTodo }) => {
   return (
     <li className={styles.item}>
       <div className={styles.content}>
-      <input type="checkbox" checked={itemProp.completed} onChange={() => handleChange(itemProp.id)} />
+      <input type="checkbox" checked={itemProp.completed} onChange={() => handleChange(itemProp.id)}/>
+      <button onClick={handleEditing}>Edit</button>
       <button onClick={() => delTodo(itemProp.id)}>Delete</button>
       <span style={itemProp.completed ? completedStyle : null}>
         {itemProp.title}
       </span>
       </div>
+      {editing ? <input type="text" value={itemProp.title} className={styles.textInput} onChange={(e) => console.log(e.target.value, itemProp.id)}/> : null}
     </li>
   )
 }
