73ade91b1147d4fddc3635b88968b1beb9ed08ca
jonas-45 <jonaswullah@gmail.com>
2023-02-28 17:33:33 +0000

    Add react-redux linter configuration

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@/styles/app.css'
 import TodoApp from "@/components/TodoApp";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
 root.render(
   <React.StrictMode>
-    <TodoApp />
+    <Router>
+      <TodoApp />
+    </Router>
   </React.StrictMode>
 );
